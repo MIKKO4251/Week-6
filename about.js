b1b3d24654cd37a502cc6c2dbef5c30ff4f60971
mikko4251 <mikelakanoh@gmail.com>
2024-04-18 14:47:47 +0100

    fix: added style

--- about.js
+++ about.js
@@ -1,29 +1,61 @@
 const idIntro = document.getElementById('intro')
 
-idIntro.innerHTML = '<strong>Introduction:</strong> My name is MIkko, I do business and a digital product designer'
+idIntro.innerHTML = '<strong>Introduction:</strong> My name is MIkko, I do business and a digital product designer.'
+
+idIntro.style.fontSize = '30px'
+idIntro.style.paddingInline = '20px'
 
 const pTwo = document.getElementsByClassName('body')
 
 pTwo[0].innerHTML = '<strong>Passion and Interests:</strong> I have a passion for designing and interest in business.'
 
+pTwo[0].style.fontSize = '30px'
+pTwo[0].style.paddingInline = '20px'
+
 
 pTwo[1].innerHTML = '<strong>Professional Background:</strong> With a background of Human Resources Management and a skill in software development, product design precisely.'
 
+pTwo[1].style.fontSize = '30'
+pTwo[1].style.paddingInline = '20px'
 
 pTwo[2].innerHTML = '<strong>Education:</strong> Bsc in Human resources management at Michael Okpara University of Agriculture.'
+
+pTwo[2].style.fontSize = '30'
+pTwo[2].style.paddingInline = '20px'
 
 const pTag = document.getElementsByTagName('p')
 
 pTag[4].innerHTML = '<strong>Projects and Contributions:</strong> I have designed so many projects that has been really useful to companies and small businesses.'
 
+pTag[4].style.fontSize = '30'
+pTag[4].style.paddingInline = '20px'
+
 const pTag1 = document.getElementsByTagName('p')
 
-pTag1[5].innerHTML = '<strong>Skills and Expertise:</strong> '
+pTag1[5].innerHTML = '<strong>Skills and Expertise:</strong> Empathy Mapping, User research, Userflow, prototyping.'
+
+pTag[5].style.fontSize = '30'
+pTag[5].style.paddingInline = '20px'
 
 const pTag2 = document.getElementsByTagName('p')
 
-pTag2[6].innerText = 'Hobbies: My hobbies are: \n music \n Designing'
+pTag2[6].innerText = 'Hobbies: My hobbies are:\n music \n Designing'
+
+pTag2[6].style.fontSize = '30'
+pTag2[6].style.paddingInline = '20px'
+
+
 
 const header = document.querySelector(h1)
 
+header.style.color = 'aqua'
+header.style.textAlign = 'center'
+header.style.border = '5px solid'
+header.style.backgroundColor = 'white'
+header.style.margin = '0'
+header.style.paddingInline = '30px'
+header.style.fontSize = '30px'
+
 const body = document.querySelector('body')
+body.style.backgroundColor = 'blue'
+body.style.color = 'white'
